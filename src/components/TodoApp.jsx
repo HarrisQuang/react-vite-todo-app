912c597b44312a0132d57252d091ba971be3405f
kimhai <kimhaiareal@gmail.com>
2023-06-15 22:19:10 +0700

    commit

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -2,12 +2,14 @@ import Header from '@/components/Header';
 import TodosLogic from './TodosLogic';
 import Navbar from './Navbar';
 import Modal from './Modal';
+import Form from './Form';
 
 const TodoApp = () => {
     return (
         <>
             <Navbar />
             <Modal />
+            <Form />
             {/* <Header />
             <TodosLogic /> */}
         </>
